ed4c3984e4e7246be9a1c5b0860b50f81b22769f
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-10 01:38:38 +0530

    Update Project.tsx

--- client/Components/Main/Project.tsx
+++ client/Components/Main/Project.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Project = () => {
-  return (
-    <div>Project</div>
-  )
+    return (
+        <div className="flex flex-col items-center justify-center py-2" id="projects" >
+            <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+                My Projects
+            </h1>
+
+        </div>
+    )
 }
 
 export default Project
