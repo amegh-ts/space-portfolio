09397d05ceefd5a3e5087dbe46d2f86cec0bb19e
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-10 00:44:51 +0530

    Update layout.tsx

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
-        <StarsCanvas/>
+        <StarsCanvas />
         {children}</body>
     </html>
   );
