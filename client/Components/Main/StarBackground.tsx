89ed7e0174c57c2fde1934a509be28155d6f509a
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-10 00:38:45 +0530

    10-02-2024

---
+++ client/Components/Main/StarBackground.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React, { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+// @ts-ignore
+import * as random from "maath/random/dist/maath-random.esm";
+
+const StarBackground = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default StarBackground
