01fff769918acffe2b1c55ed062f2430c01f442a
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-10 02:50:58 +0530

    Update Footer.tsx

--- client/Components/Main/Footer.tsx
+++ client/Components/Main/Footer.tsx
@@ -11,7 +11,7 @@ import { FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
+    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] m-top-[20px]">
         <div className="w-full flex flex-col items-center justify-center m-auto">
             <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
                 
