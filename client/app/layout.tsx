5484f30798aff0bc8d7fe01a39b8f20875d56f91
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-10 01:06:43 +0530

    10-02-2024

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StarsCanvas from "@/Components/Main/StarBackground";
-import Navbar from "@/Components/Main/NavBar";
+import Navbar from "@/Components/Main/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
