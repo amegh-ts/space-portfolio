5773113d3071f29d8d44ae1c6fdd0319b4d255f8
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-10 01:28:44 +0530

    Update Encryption.tsx

--- client/Components/Main/Encryption.tsx
+++ client/Components/Main/Encryption.tsx
@@ -21,8 +21,20 @@ const Encryption = () => {
 
             <div className="flex flex-col items-center justify-center translate-y-[-50px] absolute z-[20] w-auto h-auto">
                 <div className="flex flex-col items-center group cursor-pointer w-auto h-auto">
-
+                    <Image src="/LockTop.png" alt="Lock top" width={50} height={50} className="w-[50px] translate-y-5 transition-all duration-200 group-hover:translate-y-11" />
+                    <Image src="/LockMain.png" alt="Lock main" width={70} height={70} className="z-10" />
                 </div>
+            </div>
+
+            <div className="w-full flex items-start justify-center absolute">
+                <video loop
+                    muted
+                    autoPlay
+                    playsInline
+                    preload="false"
+                    className="w-full h-auto"
+                    src="/encryption.webm/"
+                />
             </div>
         </div>
     )
