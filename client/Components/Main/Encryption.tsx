158a6c9a855e0f84bd63e3d188400c04200798f7
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-10 01:36:19 +0530

    10-02-2024

--- client/Components/Main/Encryption.tsx
+++ client/Components/Main/Encryption.tsx
@@ -24,10 +24,19 @@ const Encryption = () => {
                     <Image src="/LockTop.png" alt="Lock top" width={50} height={50} className="w-[50px] translate-y-5 transition-all duration-200 group-hover:translate-y-11" />
                     <Image src="/LockMain.png" alt="Lock main" width={70} height={70} className="z-10" />
                 </div>
+                <div className="Welcome-box px-[15px] py-[4px] z-[20] brder my-[20px] border-[#7042f88b] opacity-[0.9]">
+                    <h1 className="Welcome-text text-[12px]">Encryption</h1>
+                </div>
             </div>
 
+            <div className="absolute z-[20] bottom-[10px] px-[5px]">
+                <div className="cursive text-[20px] font-medium text-center text-gray-300">
+                    Secure your data with end-to-end encryption
+                </div>
+            </div>
+            
             <div className="w-full flex items-start justify-center absolute">
-                <video loop muted autoPlay playsInline preload="false" className="w-full h-auto" src="/encryption.webm/" />
+                <video loop muted autoPlay playsInline preload="false" className="w-full h-auto" src="/encryption.webm" />
             </div>
         </div>
     )
---
+++ client/Components/Sub/ProjectCard.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+
+interface Props {
+  src: string;
+  title: string;
+  description: string;
+}
+
+const ProjectCard = ({ src, title, description }: Props) => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default ProjectCard
