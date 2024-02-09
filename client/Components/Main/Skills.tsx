eea15184ae7d19e38247c4fd6c7e77edde706deb
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-10 01:11:10 +0530

    10-02-2024

--- client/Components/Main/Skills.tsx
+++ client/Components/Main/Skills.tsx
@@ -8,6 +8,18 @@ const Skills = () => {
             style={{ transform: "scale(0.9" }}
         >
 
+            <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+                {Skill_data.map((image, index) => (
+                    <SkillDataProvider
+                        key={index}
+                        src={image.Image}
+                        width={image.width}
+                        height={image.height}
+                        index={index}
+                    />
+                ))}
+            </div>
+
         </section>
     )
 }
---
+++ client/Components/Sub/SkillDataProvider.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import React from 'react'
+import { motion } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+
+interface Props {
+    src: string;
+    width: number;
+    height: number;
+    index: number;
+}
+
+const SkillDataProvider = ({ src, width, height, index }: Props) => {
+    const { ref, inView } = useInView({
+        triggerOnce: true
+    })
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default SkillDataProvider
