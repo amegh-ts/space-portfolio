9e76716cd84accf851714318f51c2e50147601e8
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-02-10 00:41:26 +0530

    Update StarBackground.tsx

--- client/Components/Main/StarBackground.tsx
+++ client/Components/Main/StarBackground.tsx
@@ -11,7 +11,10 @@ const StarBackground = (props: any) => {
     const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5000), { radius: 1.2 })
   );
-  
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta/10;
+    ref.current.rotation.y -= delta/15;
+  })
   return (
     <div>
 
